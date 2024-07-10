48da6e1d95e4b072012d5172e99956a3d4134923
Gabriel-S-Santana <gabrielsouzasantana2@gmail.com>
2024-07-10 10:01:31 -0300

    estilizando hero + animando navbar

--- dist/scripts/navBar.js
+++ dist/scripts/navBar.js
@@ -1,10 +1,16 @@
 const btnMenu = document.getElementById('menu-button')
 const listMenu = document.querySelector('.list-menu')
 
-btnMenu.addEventListener('click', ()=>{
-    if(listMenu.style.display == 'none'){
-        listMenu.style.display = 'flex'
-    }else {
-        listMenu.style.display = 'none'
+btnMenu.addEventListener('click', () => {
+    if (!listMenu.classList.contains('active')) {
+        listMenu.style.display = 'flex'; // Exibe o menu antes de adicionar a classe
+        setTimeout(() => {
+            listMenu.classList.add('active');
+        }, 10); // Adiciona a classe após um pequeno atraso
+    } else {
+        listMenu.classList.remove('active');
+        setTimeout(() => {
+            listMenu.style.display = 'none'; // Esconde o menu após a animação
+        }, 80); // Tempo da animação
     }
-})
+});
